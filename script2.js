4b367522564c46fbdc1d9a6a43a6a2f6abf40452
Mhirra <mhirralunayung@gmail.com>
2017-10-04 02:38:38 -0500

    Add files via upload
    Was not able to get script.js file thus starting new with script2.js and have comments as to show planned work.

---
+++ script2.js
@@ -0,0 +1,15 @@
+// Wait for page to load
+$(document).ready(function(){
+
+// Show an initial array of topics
+var topics = ['orchid', 'sunflower', 'peony'];
+
+// Generic function for displaying topic data 
+function renderButtons()
+{
+
+// Loop the array of topics and create buttons for each array
+
+// Clicking on button calls API and show 10 max results per client and presents GIPHY and rating
+
+// Stop/Go Animation
